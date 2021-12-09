f4c6aa4282ccf2ef87c9851cf4721976807e8877
Antonio Dolce <dolce.antonio@gmail.com>
2021-12-09 22:38:26 +0100

    added lambda function

--- index.ts
+++ index.ts
@@ -2,8 +2,24 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+// function from gitlab package repo
+const functionZipUrl = "https://gitlab.com/api/v4/packages/maven/ch/dulce/quarkus-lambda-gitlab/1.0/quarkus-lambda-gitlab-1.0-bin.zip";
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+// Configure IAM so that the AWS Lambda can be run.
+const role = new aws.iam.Role('functionRole', {
+    assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({
+        Service: "lambda.amazonaws.com"
+    })
+});
+
+// Next, create the Lambda function itself:
+const docsHandlerFunc = new aws.lambda.Function("docsHandlerFunc", {
+    code: new pulumi.asset.RemoteArchive(functionZipUrl),
+    runtime: aws.lambda.CustomRuntime,
+    role: role.arn,
+    handler: "na"
+});
+
+// Export the function
+export const functionArn = docsHandlerFunc.arn;
+
